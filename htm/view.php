022482f1e7a20aa0d0cc59b38e8d8654fe553197
meenmon4455 <71626613+meenmon4455@users.noreply.github.com>
2021-02-19 22:57:22 +0700

    Add files via upload

--- htm/view.php
+++ htm/view.php
@@ -16,40 +16,52 @@
 </form>
 <?php
 // connect to the database
-$conn=mysqli_connect("localhost", "root", "","RegisterDB");
+$conn=mysqli_connect("localhost", "root", "","volunteerhub");
 $conn->query("SET NAMES UTF8");
 // get results from database
 if (isset($_GET["search"])) {
- $name = $_GET["search"];
- $sql="SELECT * FROM register WHERE firstname LIKE '$name'";
+ $title = $_GET["search"];
+ $sql="SELECT * FROM volunteer WHERE title LIKE '$title'";
 } else {
- $sql="SELECT * FROM register";
+ $sql="SELECT * FROM volunteer";
 }
 $rs=$conn->query($sql);
 // Print Header of Table
 echo "<table border='1' cellpadding='10' width=80% >";
 echo "<tr>
 <th>ID</th>
-<th>First Name</th>
-<th>Last Name</th>
-<th>Age</th>
-<th>Gender</th>
-<th>Photo</th>
-<th></th>
+<th>Title</th>
+<th>Description</th>
+<th>Attendants Target</th>
+<th>Attendants</th>
+<th>Due Date</th>
+<th>Detail</th>
+<th>Expense</th>
+<th>Advantage</th>
+<th>Type</th>
+<th>Image Banner</th>
+<th>Image</th>
  </tr>";
  // loop through results of database query, displaying them in the table
 while($row = $rs->fetch_assoc()) {
-$id = $row['ID'];
+$id = $row['id'];
 $data = "return alertID($id)";
 
 // echo out the contents of each row into a table
 echo "<tr>";
 echo '<td>' . $id . '</td>';
-echo '<td>' . $row['FirstName'] . '</td>';
-echo '<td>' . $row['LastName'] . '</td>';
-echo '<td>' . $row['Age'] . '</td>';
-echo '<td>' . $row['Gender'] . '</td>';
-echo '<td>' . '<img src="'.$row['Photo'].'" width = "25">'. '</td>';
+echo '<td>' . $row['id'] . '</td>';
+echo '<td>' . $row['title'] . '</td>';
+echo '<td>' . $row['description'] . '</td>';
+echo '<td>' . $row['attendants_target'] . '</td>';
+echo '<td>' . $row['attendants'] . '</td>';
+echo '<td>' . $row['due_date'] . '</td>';
+echo '<td>' . $row['detail'] . '</td>';
+echo '<td>' . $row['expenses'] . '</td>';
+echo '<td>' . $row['advantage'] . '</td>';
+echo '<td>' . $row['type'] . '</td>';
+echo '<td>' . '<img src="'.$row['img_banner'].'" width = "25">'. '</td>';
+echo '<td>' . '<img src="'.$row['img'].'" width = "25">'. '</td>';
 echo '<td><a href="editForm.php?id='.$id.'">Edit</a> ';
 echo '<a href="delete.php?id='.$id.'">Delete</a></td>';
 echo "</tr>";
