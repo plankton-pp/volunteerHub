fa675bec20877d7b307f423fbef3918e4aeaa38f
meenmon4455 <71626613+meenmon4455@users.noreply.github.com>
2021-02-21 16:09:29 +0700

    fixdowloaddoc bug

--- htm/downloaddoc.php
+++ htm/downloaddoc.php
@@ -1,14 +1,16 @@
-<?php 
-  
-$file = $_GET["file"] .".pdf"; 
-  
-// We will be outputting a PDF 
-header('Content-Type: application/pdf'); 
-  
-// It will be called downloaded.pdf 
-header('Content-Disposition: attachment; filename="doc.pdf"'); 
-  
-$imagpdf = file_put_contents($image, file_get_contents($file));  
-  
-echo $imagepdf; 
-?> 
+<?php
+$name= $_GET['nama'];
+
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/force-download');
+    header("Content-Disposition: attachment; filename=\"" . basename($name) . "\";");
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($name));
+    ob_clean();
+    flush();
+    readfile("upload/".$name); //showing the path to the server where the file is to be download
+    exit;
+?>
--- htm/index.php
+++ htm/index.php
@@ -45,7 +45,7 @@ session_start();
 	          <a class="nav-link" href="viewdoc.php">viewdoc</a>
 	        </li>
 	        <li class="nav-item">
-	          <a class="nav-link" href="downloaddoc.php">DowloadDoc</a>
+	          <a class="nav-link"  href="downloaddoc.php?nama=doc.pdf" >Dowload</a>
 	        </li>
 	      </ul>
 	      <form class="d-flex">
