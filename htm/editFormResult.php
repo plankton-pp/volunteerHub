29f58059056ee0b3907b3f3d7f431a96001a200e
Plankton :) <65739430+plankton-pp@users.noreply.github.com>
2021-02-22 22:43:48 +0700

    sum

--- htm/editFormResult.php
+++ htm/editFormResult.php
@@ -1,29 +1,128 @@
-<?php
-session_start();
-$num=$_GET['id'];
-	$conn=mysqli_connect("localhost", "root", "","volunteerhub");
-	$conn->query("SET NAMES UTF8"); 
-	 $sql_result = "SELECT * FROM register WHERE id = '".$num."' ";
-	$query_result= mysqli_query($conn,$sql_result);
-	              
-	$result_selected = mysqli_fetch_array($query_result);
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
 
-	if(!$result_selected){
-		echo "Not found ID=".$num;
-	}else{
-	  //echo "error check id=".$result['ID'];              
-	}
-	$_SESSION['result_id']=$result_selected['id'];
-	$_SESSION['result_title']=$result_selected['title'];
-	$_SESSION['result_description']=$result_selected['description'];
-	$_SESSION['result_attendants_target']=$result_selected['attendants_target'];
-	$_SESSION['result_attendants']=$result_selected['attendants'];
-	$_SESSION['result_duedate']=$result_selected['duedate'];
-	$_SESSION['result_detail']=$result_selected['detail'];
-	$_SESSION['result_expenses']=$result_selected['expenses'];
-	$_SESSION['result_advantage']=$result_selected['advantage'];
-	$_SESSION['result_type']=$result_selected['type'];
-	$_SESSION['result_img_banner']=$result_selected['img_banner'];
-	$_SESSION['result_img']=$result_selected['img'];
-	$_SESSION['result_status']=$result_selected['status'];
-?>
+                     <button type="button" data-toggle="modal"  class="btn btn-info btn-lg">Edit Detail</button>
+                     <button type="button" data-toggle="modal" data-target="#myModal2" class="btn btn-info btn-lg">Edit Document & Image</button> 
+                </div>
+                <br /><br />  
+                
+                <br />  
+                <br />  
+           </div>  
+           <br />  
+
+ <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+        &nbsp;
+          <h4 class="modal-title" id="myModalLabel">Load Secure Document</h4>
+           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+           </div>
+              <div class="modal-body">
+              <form method='post' action='' enctype="multipart/form-data" id="my-form">
+                <!--Detail Information-->
+                  <h4><b>Detail Information</b></h4>
+                  <!--Title-->
+                    <label for="userid">ชื่อโครงการ</label><br/>
+                    <input type="text" name="title" size="20" style="width: 400px;" placeholder="Title" value="<?php echo $result['title'];?>" class='form-control'required/><br>
+                  <!--Description-->
+                    <label for="description">คำอธิบายโครงการ</label><br/>
+                    <textarea name="description" class="form-control" style="width: 600px;" cols="40" rows="10"  size="20" placeholder="Description" required><?php echo $result['description'];?></textarea><br>
+                  <!--Detail-->
+                    <label for="detail">รายละเอียดกิจกรรม</label><br/>
+                    <textarea name="detail" class="form-control" style="width: 600px;" cols="40" rows="10"  size="20" placeholder="Detail" required><?php echo $result['detail'];?></textarea><br>
+                  <!--Description-->
+                    <label for="expenses">ค่าใช้จ่าย</label><br/>
+                    <textarea name="expenses" class="form-control" style="width: 600px;" cols="40" rows="10"  size="20" placeholder="Expenses" required><?php echo $result['expenses'];?></textarea>
+                    <br/>
+                  <input type='button' class='btn btn-info' value='Upload' id='upload'>
+              </form>
+
+                <!-- Preview-->
+              <div id='preview'></div>
+              </div>
+
+              <div class="modal-body3">
+              </div>
+
+              <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+      </div>
+   </div>
+</div> 
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+        &nbsp;
+          <h4 class="modal-title" id="myModalLabel">Load Secure Document</h4>
+           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+           </div>
+              <div class="modal-body">
+              <form method='post' action='' enctype="multipart/form-data" id="my-form">
+                  <!--Detail Information-->
+                  <h4><b>Document Information</b></h4>
+                  <!--Image-->
+                  <label for="file">ภาพประกอบกิจกรรม</label>
+                  <input type='file' name='img' id='img' value="" class='form-control' ><br>
+                  <!--banner-->
+                  <label for="file">ภาพปกกิจกรรม</label>
+                  <input type='file' name='img_banner' id='img_banner' class='form-control' ><br>
+                   <!--doc-->
+                  <label for="file">หนังสือขอนุญาตจัดกิจกรรม</label>
+                  <input type='file' name='img_banner' id='img_banner' class='form-control' ><br>
+
+                  <input type='button' class='btn btn-info' value='Upload' id='upload'>
+              </form>
+
+                <!-- Preview-->
+              <div id='preview'></div>
+              </div>
+
+              <div class="modal-body3">
+              </div>
+
+              <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+      </div>
+   </div>
+</div> 
+ <script>  
+$(document).ready(function(){
+  $('#upload').click(function(){
+
+    event.preventDefault();
+     var form = $("#my-form")[0];
+     var data = new FormData(form);
+    // AJAX request
+    $.ajax({
+      url: 'upload2.php',
+      type: 'post',
+      data: data,
+      contentType: false,
+      processData: false,
+      success: function(response){
+        console.log(response);
+        if(response != 0){
+          // Show image preview
+          $('#preview').html(" Process Started");
+        }else{
+          alert('file not uploaded');
+        }
+      },
+      error: function(error){
+        console.log(error);
+      }
+    });
+  });
+});
+ </script> 
+</body>
+</html>
